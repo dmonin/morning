aa05d34784964e3652027c9004910416c0a3f24c
Dmitry Monin <dmitry@17grad.com>
2015-05-20 18:13:49 +0200

    Adding Swiper-JS externs.

---
+++ externs/swiper-externs.js
@@ -0,0 +1,49 @@
+/**
+ * @fileoverview Externs for the Swiper
+ * @externs
+ */
+
+/**
+ * @param {Element} element
+ * @param {Object} config
+ * @constructor
+ */
+var Swiper = function(element, config) {};
+
+/**
+ *
+ */
+Swiper.prototype.destroy = function() {};
+
+/**
+ *
+ */
+Swiper.prototype.swipeNext = function() {};
+
+/**
+ *
+ */
+Swiper.prototype.swipePrev = function() {};
+
+
+/**
+ * @type {number}
+ */
+Swiper.prototype.clickedSlideIndex = 0;
+
+/**
+ * @type {Element}
+ */
+Swiper.prototype.clickedSlide = null;
+
+
+/**
+ * @return {Element}
+ */
+Swiper.prototype.activeSlide = function() {
+
+};
+/**
+ * @type {number}
+ */
+Swiper.prototype.activeLoopIndex = 0;
