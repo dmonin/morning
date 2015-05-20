f15bdbbdb009a7f7fe80a0cf940c3d21540be15d
Dmitry Monin <dmitry@17grad.com>
2015-05-20 18:14:39 +0200

    Changing tap state class of the arrow navigation buttons.

--- src/monin/ui/ArrowNavigation.js
+++ src/monin/ui/ArrowNavigation.js
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS-IS" BASIS,
@@ -21,6 +21,7 @@ goog.provide('monin.ui.ArrowNavigationEvent');
 goog.require('goog.dom.classlist');
 goog.require('goog.ui.registry');
 goog.require('goog.ui.Component');
+goog.require('monin.events.TapProvider');
 
 /**
  * Arrow Navigation component
@@ -30,64 +31,67 @@ goog.require('goog.ui.Component');
  */
 monin.ui.ArrowNavigation = function()
 {
-    goog.base(this);
+  goog.base(this);
 
-    /**
-     * Previous button element
-     *
-     * @type {Element}
-     * @private
-     */
-    this.prev_ = null;
+  /**
+   * Previous button element
+   *
+   * @type {Element}
+   * @private
+   */
+  this.prev_ = null;
 
-    /**
-     * Next button element
-     *
-     * @type {Element}
-     * @private
-     */
-    this.next_ = null;
+  /**
+   * Next button element
+   *
+   * @type {Element}
+   * @private
+   */
+  this.next_ = null;
 };
 goog.inherits(monin.ui.ArrowNavigation, goog.ui.Component);
 
 /** @inheritDoc */
 monin.ui.ArrowNavigation.prototype.createDom = function()
 {
-    var dom = this.getDomHelper(),
-        el = dom.createDom('div', 'arrow-nav', [
-            dom.createDom('div', 'nav-prev'),
-            dom.createDom('div', 'nav-next')
-        ]);
+  var dom = this.getDomHelper(),
+    el = dom.createDom('div', 'arrow-nav', [
+      dom.createDom('div', 'nav-prev'),
+      dom.createDom('div', 'nav-next')
+    ]);
 
-    this.decorateInternal(el);
+  this.decorateInternal(el);
 };
 
 /** @inheritDoc */
 monin.ui.ArrowNavigation.prototype.decorateInternal = function(el)
 {
-    goog.base(this, 'decorateInternal', el);
+  goog.base(this, 'decorateInternal', el);
 
-    this.prev_ = el.querySelector('.nav-prev');
-    this.next_ = el.querySelector('.nav-next');
+  this.prev_ = el.querySelector('.nav-prev');
+  this.next_ = el.querySelector('.nav-next');
+
+  this.registerDisposable(monin.events.TapProvider.attach(this.prev_));
+  this.registerDisposable(monin.events.TapProvider.attach(this.next_));
 };
 
 
 /** @inheritDoc */
 monin.ui.ArrowNavigation.prototype.enterDocument = function()
 {
-    goog.base(this, 'enterDocument');
-    var el = this.getElement();
+  goog.base(this, 'enterDocument');
+  var el = this.getElement();
 
-    if (goog.userAgent.MOBILE)
-    {
-        this.getHandler().listen(el, goog.events.EventType.TOUCHSTART,
-            this.handleMouseDown_);
-    }
-    else
-    {
-        this.getHandler().listen(el, goog.events.EventType.MOUSEDOWN,
-            this.handleMouseDown_);
-    }
+  if (goog.userAgent.MOBILE)
+  {
+    this.getHandler().listen(el, goog.events.EventType.TOUCHSTART,
+      this.handleMouseDown_);
+  }
+  else
+  {
+    this.getHandler().listen(el, goog.events.EventType.MOUSEDOWN,
+      this.handleMouseDown_);
+  }
 
 
 };
@@ -100,31 +104,31 @@ monin.ui.ArrowNavigation.prototype.enterDocument = function()
  */
 monin.ui.ArrowNavigation.prototype.handleMouseDown_ = function(e)
 {
-    if (!goog.dom.classlist.contains(e.target, 'active') || e.button == 2)
-    {
-        return;
-    }
+  if (!goog.dom.classlist.contains(e.target, 'active') || e.button == 2)
+  {
+    return;
+  }
 
-    e.preventDefault();
+  e.preventDefault();
 
-    var dir = 0;
-    if (goog.dom.classlist.contains(e.target, 'nav-prev'))
-    {
-        dir = -1;
-    }
-    else if (goog.dom.classlist.contains(e.target, 'nav-next'))
-    {
-        dir = 1;
-    }
+  var dir = 0;
+  if (goog.dom.classlist.contains(e.target, 'nav-prev'))
+  {
+    dir = -1;
+  }
+  else if (goog.dom.classlist.contains(e.target, 'nav-next'))
+  {
+    dir = 1;
+  }
 
-    if (dir !== 0)
-    {
-        var evt = new monin.ui.ArrowNavigationEvent(
-                        goog.ui.Component.EventType.ACTION, this);
-        evt.dir = dir;
+  if (dir !== 0)
+  {
+    var evt = new monin.ui.ArrowNavigationEvent(
+            goog.ui.Component.EventType.ACTION, this);
+    evt.dir = dir;
 
-        this.dispatchEvent(evt);
-    }
+    this.dispatchEvent(evt);
+  }
 };
 
 /**
@@ -135,8 +139,8 @@ monin.ui.ArrowNavigation.prototype.handleMouseDown_ = function(e)
  */
 monin.ui.ArrowNavigation.prototype.setActive = function(left, right)
 {
-    goog.dom.classlist.enable(this.prev_, 'active', left);
-    goog.dom.classlist.enable(this.next_, 'active', right);
+  goog.dom.classlist.enable(this.prev_, 'active', left);
+  goog.dom.classlist.enable(this.next_, 'active', right);
 };
 
 /**
@@ -146,34 +150,34 @@ monin.ui.ArrowNavigation.prototype.setActive = function(left, right)
  */
 monin.ui.ArrowNavigation.prototype.setVisible = function(isVisible)
 {
-    goog.dom.classlist.enable(this.getElement(), 'visible', isVisible);
+  goog.dom.classlist.enable(this.getElement(), 'visible', isVisible);
 };
 
 /**
  * Register this control so it can be created from markup.
  */
 goog.ui.registry.setDecoratorByClassName(
-    'arrow-nav',
-    function() {
-      return new monin.ui.ArrowNavigation();
-    });
+  'arrow-nav',
+  function() {
+    return new monin.ui.ArrowNavigation();
+  });
 
 
 /**
  * @constructor
  * @param {string} type Event Type.
  * @param {Object=} opt_target Reference to the object that is the target of
- *     this event. It has to implement the {@code EventTarget} interface
- *     declared at {@link http://developer.mozilla.org/en/DOM/EventTarget}.
+ *   this event. It has to implement the {@code EventTarget} interface
+ *   declared at {@link http://developer.mozilla.org/en/DOM/EventTarget}.
  * @extends {goog.events.Event} e
  */
 monin.ui.ArrowNavigationEvent = function(type, opt_target)
 {
-    goog.base(this, type, opt_target);
+  goog.base(this, type, opt_target);
 
-    /**
-     * @type {number}
-     */
-    this.dir = 0;
+  /**
+   * @type {number}
+   */
+  this.dir = 0;
 };
 goog.inherits(monin.ui.ArrowNavigationEvent, goog.events.Event);
