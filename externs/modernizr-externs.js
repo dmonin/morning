82db01d4f524830920835464c8018c9d587794ba
Dmitry Monin <dmitry@17grad.com>
2015-06-22 19:58:56 +0200

    Adding initial (needed) externs for modernizr

---
+++ externs/modernizr-externs.js
@@ -0,0 +1,44 @@
+/**
+ * @type {Object}
+ */
+var Modernizr = {};
+
+/**
+ * @type {boolean}
+ */
+Modernizr.boxshadow = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.canvas = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.csstransforms = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.csstransforms3d = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.draganddrop = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.filereader = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.video = false;
+
+/**
+ * @type {boolean}
+ */
+Modernizr.csscolumns = false;
