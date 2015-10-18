1a6c8a70f803fbb672f217c952ef3b933218bc6e
Dmitry Monin <dmitry@17grad.com>
2015-10-18 17:16:20 +0200

    Updating Swiper to the newest version.

--- src/morning/ui/Swiper.js
+++ src/morning/ui/Swiper.js
@@ -56,7 +56,7 @@ morning.ui.Swiper.loaded = false;
 /**
  * @type {string}
  */
-morning.ui.Swiper.scriptUrl = 'asset/swiper/idangerous.swiper.min.js';
+morning.ui.Swiper.scriptUrl = '/asset/morning/third-party/swiper.min.js';
 
 /** @inheritDoc */
 morning.ui.Swiper.prototype.decorateInternal = function(el)
@@ -72,18 +72,10 @@ morning.ui.Swiper.prototype.decorateInternal = function(el)
     goog.dom.dataset.get(el, 'paginationclickable') == 'true';
 
   var slidesPerView = goog.dom.dataset.get(el, 'slidesperview') || null;
-  var watchActiveIndex = goog.dom.dataset.get(el, 'watchactive') == 'true';
 
   var cfg = {
-    'mode': 'horizontal',
     'loop': loop,
-    'onSlideTouch': goog.bind(this.onSlideTouch, this),
-    'onSlideClick': goog.bind(this.onSlideClick, this),
-    'onTouchMove': goog.bind(this.onTouchMove, this),
-    'onTouchEnd': goog.bind(this.onTouchEnd, this),
-    'onSlideChangeEnd': goog.bind(this.onSlideChangeEnd, this),
-    'paginationClickable': paginationClickable,
-    'onSetWrapperTransform': this.onSetWrapperTransform.bind(this)
+    'paginationClickable': paginationClickable
   };
 
   if (pagination)
@@ -128,49 +120,6 @@ morning.ui.Swiper.prototype.disposeInternal = function()
 };
 
 /**
- * Returns currently active slide
- *
- * @return {Element}
- */
-morning.ui.Swiper.prototype.getActiveSlide = function()
-{
-  return this.swiper_.activeSlide();
-};
-
-/**
- * Returns the touched/clicked slide (slide instance, HTMLElement). For use only
- * with "onSlideTouch" and "onSlideClick" callbacks.
- *
- * @return {Element}
- */
-morning.ui.Swiper.prototype.getClickedSlide = function()
-{
-  return this.swiper_ ? this.swiper_.clickedSlide : null;
-};
-
-/**
- * Returns index of currently displayed slide
- *
- * @return {number}
- */
-morning.ui.Swiper.prototype.getActiveIndex = function()
-{
-  return this.swiper_.activeLoopIndex;
-};
-
-
-/**
- * Returns the index number of touched/clicked slide. For use only with
- * "onSlideTouch" and "onSlideClick" callbacks.
- *
- * @return {number}
- */
-morning.ui.Swiper.prototype.getClickedSlideIndex = function()
-{
-  return this.swiper_.clickedSlideIndex;
-};
-
-/**
  * @param  {goog.events.Event} e
  * @private
  */
@@ -189,74 +138,6 @@ morning.ui.Swiper.prototype.load_ = function()
 };
 
 /**
- * Handles an event when Swiper is moved
- *
- * @param  {morning.ui.Swiper} swiper
- * @param  {Object} coords
- */
-morning.ui.Swiper.prototype.onSetWrapperTransform = function(swiper, coords)
-{
-  this.dispatchEvent({
-    type: morning.ui.Swiper.EventType.SET_WRAPPER_TRANSFORM,
-    coords: {
-      x: coords['x'],
-      y: coords['y'],
-      z: coords['z']
-    },
-    width: this.swiper_ ? this.swiper_.width : 0
-  });
-};
-
-/**
- * @param  {goog.events.Event} e
- */
-morning.ui.Swiper.prototype.onSlideClick = function(e)
-{
-  this.dispatchEvent(goog.events.EventType.CLICK);
-};
-
-/**
- * @param  {goog.events.Event} e
- */
-morning.ui.Swiper.prototype.onSlideTouch = function(e)
-{
-  this.dispatchEvent(goog.events.EventType.TOUCHSTART);
-};
-
-/**
- * @param  {goog.events.Event} e
- */
-morning.ui.Swiper.prototype.onTouchMove = function(e)
-{
-  this.dispatchEvent(goog.events.EventType.TOUCHMOVE);
-};
-
-
-/**
- * @param  {goog.events.Event} e
- */
-morning.ui.Swiper.prototype.onTouchEnd = function(e)
-{
-  this.dispatchEvent(goog.events.EventType.TOUCHEND);
-};
-
-/**
- * @param  {goog.events.Event} e
- */
-morning.ui.Swiper.prototype.onSlideChangeEnd = function(e)
-{
-  this.dispatchEvent(morning.ui.Swiper.EventType.SLIDE_CHANGE_END);
-};
-
-/**
- * Swipes to next item
- */
-morning.ui.Swiper.prototype.reInit = function()
-{
-  this.swiper_.reInit();
-};
-
-/**
  * Sets the config
  *
  * @param {Object} cfg
@@ -264,43 +145,6 @@ morning.ui.Swiper.prototype.reInit = function()
 morning.ui.Swiper.prototype.setConfig = function(cfg)
 {
   goog.mixin(this.config_, cfg);
-};
-
-/**
- * Swipes to next item
- */
-morning.ui.Swiper.prototype.swipeNext = function()
-{
-  this.swiper_.swipeNext();
-};
-
-/**
- * Swipes to next item
- */
-morning.ui.Swiper.prototype.swipePrev = function()
-{
-  this.swiper_.swipePrev();
-};
-
-/**
- * @param  {number} index
- * @param  {number} speed
- * @param  {boolean} runCallbacks
- */
-morning.ui.Swiper.prototype.swipeTo = function(index, speed, runCallbacks)
-{
-  if (this.swiper_)
-  {
-    this.swiper_.swipeTo(index, speed, runCallbacks);
-  }
-};
-
-/**
- * @enum {string}
- */
-morning.ui.Swiper.EventType = {
-  SLIDE_CHANGE_END: 'slidechangeend',
-  SET_WRAPPER_TRANSFORM: 'setwrappertransform'
 };
 
 /**
